025bdc1ec703bff16edde9bd6b424eb45516d16a
yamugy <yamugy72@gmail.com>
2024-10-25 15:57:26 +0900

    Updates

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'student-manager-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/static/js/bundle.js',
+  '/static/js/main.chunk.js',
+  '/static/js/0.chunk.js',
+  '/static/css/main.chunk.css',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
